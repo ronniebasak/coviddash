396311fdef65ebdcbc75f5fba7423123658449b2
Sohan Basak <ronnie.basak96@gmail.com>
2020-04-12 01:32:57 +0530

    Added dist files

---
+++ covid-front/dist/precache-manifest.f5dab52be96ed6ef618e711bb1d0d84d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6360a6528755213ac48b",
+    "url": "/css/app.52247255.css"
+  },
+  {
+    "revision": "1030cda301acfe4862f9276dac378215",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34dbc3ab078cd8755a6f",
+    "url": "/js/about.1f4a33af.js"
+  },
+  {
+    "revision": "6360a6528755213ac48b",
+    "url": "/js/app.d0a4a5e2.js"
+  },
+  {
+    "revision": "b2dd0559b7eea767ce1a",
+    "url": "/js/chunk-vendors.cf8b361b.js"
+  },
+  {
+    "revision": "2a1b68ed18e90cdddf5987f16bc53c30",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
